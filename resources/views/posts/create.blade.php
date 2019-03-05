8d9b1fa6b9e150743164a745a1232524915d4e7c
Sirkarov <josif_sirkarov@hotmail.com>
2019-03-05 20:46:02 +0100

    Creating Post and importing into database

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,25 @@
+@extends('main')
+
+@section('title','| Create New Post')
+
+@section('content')
+
+        <div class="row">
+            <div class="col-md-8 offset-2">
+                <h1>Create New Post</h1>
+                <hr>
+                <form method="POST" action="{{route('posts.store')}}">
+                   @csrf
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" class="form-control" name="title" id="PostTitle" aria-describedby="emailHelp" placeholder="Enter title">
+                    </div>
+                    <div class="form-group">
+                        <label for="body">Body</label>
+                        <textarea type="text" class="form-control" name="body" id="PostBody" placeholder="Enter body for the Post" rows="5"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Submit Post</button>
+                </form>
+            </div>
+        </div>
+@endsection
