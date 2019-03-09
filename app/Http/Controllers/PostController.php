7041bcd38bc1fef5d6aae0961b48c4f08f06bb06
Sirkarov <josif_sirkarov@hotmail.com>
2019-03-09 12:58:39 +0100

    Displaying Post after creating

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -68,7 +68,8 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        return view('posts.show');
+        $post = Post::findOrFail($id);
+        return view('posts.show')->with('post',$post);
     }
 
     /**
