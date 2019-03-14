6efc2a27919abc0b2c48eff7681d404da641e5b6
Sirkarov <josif_sirkarov@hotmail.com>
2019-03-14 09:47:31 +0100

    test in posts h1

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 	<div class="row">
 		<div class="col-md-10">
-			<h1>All Posts</h1>
+			<h1>All Posts Created from Josif</h1>
 		</div>
 		<div class="col-md-2">
 			<a href="{{ route('posts.create') }}" class="btn btn-primary btn-block btn-h1-spacing">Create New Post</a>
